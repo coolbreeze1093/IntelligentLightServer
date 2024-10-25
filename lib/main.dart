5b37bca2483f5929ffa71c53ac7dd3410b129d26
wei <coolbreeze23@163.com>
2024-10-25 15:38:59 +0800

    界面开发完成

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.dark(
+          primary: Colors.blueGrey.shade800,
+          secondary: Colors.cyanAccent,
+          surface: Colors.grey.shade800,
+          error: Colors.redAccent,
+          onPrimary: Colors.white,
+          onSecondary: Colors.black,
+          onSurface: Colors.white,
+          onError: Colors.black,
+          brightness: Brightness.dark,
+        ),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,13 +43,23 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   double _brightness = 0.5;
-  Map<BrightnessModel, bool> _checkboxState = {
-    BrightnessModel.close: true,
-    BrightnessModel.breathe: false,
-    BrightnessModel.flash: false,
-    BrightnessModel.constant: false,
-  };
+
   BrightnessModel _brightnessModel = BrightnessModel.close;
+
+  // 模式的名称
+  final Map<BrightnessModel, String> _showText = {
+    BrightnessModel.close: '关闭',
+    BrightnessModel.breathe: '呼吸',
+    BrightnessModel.constant: '常量',
+    BrightnessModel.flash: '闪烁'
+  };
+
+  final List<BrightnessModel> _showSeq = [
+    BrightnessModel.constant,
+    BrightnessModel.breathe,
+    BrightnessModel.flash,
+    BrightnessModel.close
+  ];
 
   void _incrementCounter(double value) {
     setState(() {
@@ -51,105 +71,93 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.surface,
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Flexible(
-                child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Flexible(
-                  child: CheckboxListTile(
-                    title: const Text("常量"),
-                    value: _checkboxState[BrightnessModel.constant],
-                    onChanged: (bool? state) {
-                      setState(() {
-                        if (state != null) {
-                          _brightnessModel = BrightnessModel.constant;
-                          setCheckBoxState(
-                              _checkboxState, _brightnessModel, state);
-                        }
-                      });
-                    },
+            Column(
+              children: [
+                const Icon(
+                  Icons.wb_sunny, // 表示亮度增强
+                  color: Colors.yellow,
+                  size: 30,
+                ),
+                SizedBox(
+                  height: 280,
+                  child: RotatedBox(
+                    quarterTurns: 3,
+                    child: SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                        trackHeight: 60, // 设置轨道高度
+                        trackShape: const RoundedRectSliderTrackShape(),
+                        activeTrackColor: Colors.cyanAccent.shade400,
+                        inactiveTrackColor: Colors.blueGrey.shade700,
+                        thumbShape: const RoundSliderThumbShape(
+                            enabledThumbRadius: 25), // 设置滑块大小
+                        overlayShape: const RoundSliderOverlayShape(
+                            overlayRadius: 30), // 设置滑块外圈大小
+                      ),
+                      child: Slider(
+                        value: _brightness,
+                        onChanged: (double value) {
+                          _incrementCounter(value);
+                          setState(() {
+                            _brightness = value;
+                          });
+                        },
+                        onChangeEnd: (double value) {},
+                        min: 0,
+                        max: 1024,
+                        divisions: 1024,
+                        label: _counter.toString(),
+                      ),
+                    ),
                   ),
                 ),
-                Flexible(
-                  child: CheckboxListTile(
-                      title: const Text("呼吸"),
-                      value: _checkboxState[BrightnessModel.breathe],
-                      onChanged: (bool? state) {
-                        setState(() {
-                          if (state != null) {
-                            _brightnessModel = BrightnessModel.breathe;
-                            setCheckBoxState(
-                                _checkboxState, _brightnessModel, state);
-                          }
-                        });
-                      }),
-                ),
-                Flexible(
-                  child: CheckboxListTile(
-                      title: const Text("闪烁"),
-                      value: _checkboxState[BrightnessModel.flash],
-                      onChanged: (bool? state) {
-                        setState(() {
-                          if (state != null) {
-                            _brightnessModel = BrightnessModel.flash;
-                            setCheckBoxState(
-                                _checkboxState, _brightnessModel, state);
-                          }
-                        });
-                      }),
-                ),
-                Flexible(
-                  child: CheckboxListTile(
-                      title: const Text("关闭"),
-                      value: _checkboxState[BrightnessModel.close],
-                      onChanged: (bool? state) {
-                        setState(() {
-                          if (state != null) {
-                            _brightnessModel = BrightnessModel.close;
-                            setCheckBoxState(
-                                _checkboxState, _brightnessModel, state);
-                          }
-                        });
-                      }),
+                const Icon(
+                  Icons.nightlight_round, // 表示亮度减弱
+                  color: Colors.blueGrey,
+                  size: 30,
                 ),
               ],
-            )),
-            const SizedBox(height: 20),
-            SizedBox(
-              height: 280,
-              child: RotatedBox(
-                  quarterTurns: 3,
-                  child: SliderTheme(
-                    data: SliderTheme.of(context).copyWith(
-                      trackHeight: 60, // 设置轨道高度
-                      thumbShape: const RoundSliderThumbShape(
-                          enabledThumbRadius: 25), // 设置滑块大小
-                      overlayShape: const RoundSliderOverlayShape(
-                          overlayRadius: 30), // 设置滑块外圈大小
-                    ),
-                    child: Slider(
-                      value: _brightness,
-                      onChanged: (double value) {
-                        _incrementCounter(value);
+            ),
+            const SizedBox(height: 100),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(25), // 设置整体圆角
+              child: Container(
+                width: 320, // 设置宽度，确保内容不超出
+                height: 50, // 设置高度
+                color: Colors.transparent,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(_showSeq.length, (index) {
+                    return GestureDetector(
+                      onTap: () {
                         setState(() {
-                          _brightness = value;
+                          _brightnessModel = _showSeq[index];
                         });
                       },
-                      min: 0,
-                      max: 1024,
-                      divisions: 1024,
-                      label: _counter.toString(),
-                    ),
-                  )),
+                      child: Container(
+                        width: 80, // 设置宽度
+                        height: 50, // 设置高度
+                        alignment: Alignment.center, // 使文字居中
+                        color: _brightnessModel == _showSeq[index]
+                            ? Colors.cyanAccent.shade700
+                            : Colors.blueGrey.shade600,
+                        child: Text(
+                          _showText[_showSeq[index]]!,
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              ),
             ),
-            const Text('Adjust Brightness'),
           ],
         ),
       ),
